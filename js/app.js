4090b14e44c51a43d3b8ec4969219bf03ac1353a
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-12 00:05:44 +0200

    ADD - milestone 3

--- js/app.js
+++ js/app.js
@@ -6,34 +6,60 @@ const images = [
     "./img/04.webp",
     "./img/05.webp",
 ]
-console.log(images)
+console.log(images);
 
-// - recuperare l'elemento "carousel" dal dom
+
+// - recuperare l"elemento "carousel" dal dom
 const carouselDOMElement = document.querySelector(".carousel")
+
 
 // - Inizializzare il ciclo for per il carosello
 for (let i = 0; i < images.length; i++){
-    let imageCarousel = images[i]
+let imageCarousel = images[i];
     
 // - concatenare un template literal
-const carouselString = `
-<div>
-    <img src="${imageCarousel}">
-</div>
-`
+const carouselString = `<img class="carousel-item" src="${imageCarousel}">`;
+
 
 // - Stampare in pagina le immagini del carosello
-carouselDOMElement.innerHTML = carouselString
+carouselDOMElement.innerHTML = carouselString + carouselDOMElement.innerHTML;
+console.log (imageCarousel, i, carouselString, carouselDOMElement)
+
 }
 
-// const btnUpString = `
-// <button>
-//     <i class="fa-solid fa-chevron-up"></i>
-// </button>
-// `
-// const btnDownString = `
-// <button>
-//     <i class="fa-solid fa-chevron-down">
-// </i></button>
-// `
+
+// - Recuperare le immagini all"interno del carousel dal dom
+const itemDOMElements = document.querySelectorAll(".carousel-item");
+
+
+// - Memorizzare lo stato del carosello
+let currentIndex = 0;
+
+
+// - Aggiungere la classe active all"immagine attiva
+let currentImg = itemDOMElements[currentIndex];
+currentImg.classList.add("active");
+console.log(currentImg)
+
+
+
+// - Dichiarare i controlli del carosello
+const btnTop = document.querySelector(".btn-up");
+const btnBottom = document.querySelector(".btn-down");
+
+btnTop.addEventListener("click", function () {
+    itemDOMElements[currentIndex].classList.remove("active");
+    currentIndex++;
+    itemDOMElements[currentIndex].classList.add("active");
+	console.log(currentImg, currentIndex);
+})
+
+btnBottom.addEventListener("click", function () {
+    itemDOMElements[currentIndex].classList.remove("active");
+    currentIndex--;
+    itemDOMElements[currentIndex].classList.add("active");
+	console.log(currentImg, currentIndex);
+})
+
+
 
